111ca8c61ac862e613c0ea29c3cee41f1cdc9807
Marco Torres <marcotorres258@outlook.com>
2023-05-08 21:22:48 -0600

    first commit

---
+++ create_sql_script/getCarData.js
@@ -0,0 +1,68 @@
+const fs = require("fs");
+
+function getCarData(fileName) {
+  const dealerships = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  // Read file and get json
+  const file = fs?.readFileSync(`../data/${fileName}`, "utf8");
+  const allData = JSON?.parse(file);
+
+  // Get relevant data
+  const main = allData?.data?.data?.mainResult;
+  const carData = {};
+
+  // ------------- Model data -------------
+  carData.brand = main?.car_make;
+  carData.model_name = main?.car_model;
+  carData.body_type = main?.body_type;
+  carData.year = main?.car_year || 2000;
+
+  // ------------- Variant data -------------
+  carData.full_name = main?.car_name;
+  carData.variant_name = main?.car_trim;
+  carData.is_new = carData?.year >= 2020 ? "TRUE" : "FALSE";
+  carData.used_km = main?.km || 0;
+  carData.fuel_type = main?.fuel_type;
+  carData.horse_power = main?.horse_power;
+  carData.passengers = main?.passengers || 0;
+  carData.traction = main?.traction;
+  carData.transmission = main?.transmission;
+  carData.spin = allData?.data?.media?.details?.spin;
+
+  // Color and images
+  const original_color = main?.ext_color;
+  const original_images =
+    allData?.data?.checkout?.static?.media?.inventoryMedia;
+  const images = [];
+  original_images?.forEach((img) => {
+    const image = {};
+    image.img_url = "https://images?.kavak?.services/" + img?.media;
+    image.order = img?.order;
+    images.push(image);
+  });
+  const color = {
+    name: original_color,
+    hex_code: "#000000",
+    images,
+  };
+  carData.colors = [color];
+
+  // ------------- Listing -------------
+  carData.dealership_id =
+    dealerships[Math?.floor(Math?.random() * dealerships?.length)];
+  carData.status = "available";
+  carData.base_price = main?.price || 0;
+  carData.available_test_drive = "TRUE";
+
+  return carData;
+}
+
+// function main() {
+//   const fileName = "13225_ok?.json";
+//   const carData = getCarData(fileName);
+//   console?.log(carData);
+// }
+
+// // main();
+
+exports.getCarData = getCarData;
---
+++ create_sql_script/index.js
@@ -0,0 +1,100 @@
+const fs = require("fs");
+const { getCarData } = require("./getCarData");
+
+const fileNames = fs.readdirSync("../data");
+
+let carModelsScript = `
+INSERT INTO public.car_models 
+(
+  brand,
+  model_name,
+  body_type,
+  year
+) 
+VALUES
+`;
+
+let carVariantsScript = `
+  INSERT INTO public.car_variant
+  (
+    car_model_id,
+    full_name,
+    variant_name,
+    is_new,
+    used_km,
+    fuel_type,
+    horse_power,
+    passengers,
+    traction,
+    transmission,
+    spin,
+    colors
+  )
+  VALUES
+`;
+
+let listingScript = `
+INSERT INTO public.listings
+(
+  dealership_id, 
+  car_model_id, 
+  car_variant_id, 
+  base_price, 
+  test_drive
+) 
+VALUES
+`;
+
+// Process data from each file
+fileNames.slice(0, 1000).forEach((fileName, i) => {
+  // Get car data
+  const carData = getCarData(fileName);
+
+  // Add data to model script
+  carModelsScript += `
+  -- ${i}
+  (
+    '${carData.brand}', 
+    '${carData.model_name}', 
+    '${carData.body_type}', 
+    ${carData.year}
+  ),`;
+
+  // Add data to variant script
+  const car_model_id = i + 1;
+  carVariantsScript += `
+  -- ${i}
+  (
+    ${car_model_id}, 
+    '${carData.full_name}', 
+    '${carData.variant_name}', 
+    ${carData.is_new}, 
+    ${carData.used_km}, 
+    '${carData.fuel_type}', 
+    '${carData.horse_power}', 
+    ${carData.passengers}, 
+    '${carData.traction}', 
+    '${carData.transmission}', 
+    '${carData.spin}',
+    array['${
+      carData.colors.length ? JSON.stringify(carData.colors[0]) : {}
+    }']::json[]
+  ),`;
+
+  // Add data to listing script
+  const car_variant_id = i + 1;
+  listingScript += `
+  -- ${i}
+  (
+    ${carData.dealership_id}, 
+    ${car_variant_id}, 
+    ${car_variant_id},
+    ${carData.base_price}, 
+    ${carData.available_test_drive}
+  ),`;
+});
+
+// Download files
+fs.writeFileSync("../results/create_models.sql", carModelsScript);
+fs.writeFileSync("../results/create_variants.sql", carVariantsScript);
+fs.writeFileSync("../results/create_listings.sql", listingScript);
